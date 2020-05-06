47168abb8f6f2e37944bf78f57eb1f5a5b71ac08
Trevor Blades <tdblades@gmail.com>
2020-05-06 13:30:38 -0700

    Add an example and refine the readme

---
+++ example/gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-embedder',
+            options: {
+              customTransformers: [require('excalidraw-embed')]
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
